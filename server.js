dc01e4a5e76aeb39eae1acc162d3e4c4c6dda1af
German Yerzhakov <gyerzhakov@cogniance.com>
2014-04-20 22:54:15 +0300

    create server

--- server.js
+++ server.js
@@ -4,26 +4,37 @@ var EE = require('events').EventEmitter;
 db.connect();
 var ee = new EE();
 
-var User = require("./user");
+var server = new require('http').Server();
 
 function run() {
-/*	var vasya = new User("Vasya");
+	server.listen(1337, '127.0.0.1');
+}
+
+server.on('request', function(req, res){
+	res.end('Helllo Gallllya!!! I love you!');
+})
+
+var User = require("./user");
+
+function learnModulesErrorEventEmitter() {
+	var vasya = new User("Vasya");
 	var petya = new User("Petya");
 	vasya.sayHello(petya);
 	console.log(db.getPhrase('a'));
-	vasya.saySomething();*/
+	vasya.saySomething();
 	ee.emit('evvvveent', 'Im event EVVVEENT!! YE!');
 	ee.emit('error', new Err());
+
+	ee.on('evvvveent', function(ev){
+		console.log("Event evvveent was fired!");
+		console.log(ev);
+	});
+
+	ee.on('error', function(err){
+		console.log("Name of error is" + err.name);
+	})
+
 }
-
-ee.on('evvvveent', function(ev){
-	console.log("Event evvveent was fired!");
-	console.log(ev);
-});
-
-ee.on('error', function(err){
-	console.log("Name of error is" + err.name);
-})
 
 
 if (module.parent) {
