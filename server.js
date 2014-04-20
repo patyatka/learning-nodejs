31de02845668258a594c5dc406df227721bd6731
German Yerzhakov <gyerzhakov@cogniance.com>
2014-04-20 13:04:34 +0300

    learning event emitter module

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
+var Err = require('./error');
 var db = require("db");
+var EE = require('events').EventEmitter;
 db.connect();
+var ee = new EE();
 
 var User = require("./user");
 
 function run() {
-	var vasya = new User("Vasya");
+/*	var vasya = new User("Vasya");
 	var petya = new User("Petya");
 	vasya.sayHello(petya);
 	console.log(db.getPhrase('a'));
-	vasya.saySomething();
+	vasya.saySomething();*/
+	ee.emit('evvvveent', 'Im event EVVVEENT!! YE!');
+	ee.emit('error', new Err());
 }
+
+ee.on('evvvveent', function(ev){
+	console.log("Event evvveent was fired!");
+	console.log(ev);
+});
+
+ee.on('error', function(err){
+	console.log("Name of error is" + err.name);
+})
 
 
 if (module.parent) {
